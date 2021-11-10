cdfbdf93571836f15a5a992a86c0b1acf347a169
DAC-info <luca.eich@thedac.info>
2021-11-10 19:39:42 +0900

    update iframe link

--- pages/liquidity.tsx
+++ pages/liquidity.tsx
@@ -14,7 +14,7 @@ const Liquidity: NextPage = () => {
             <div className="w-full -mb-4 xs:-mb-6 sm:-mb-10 xl:-mb-0 h-full flex flex-col items-center justify-center animate__animated animate__fadeIn animate__slow">
 
                 <iframe
-                    src="https://app.uniswap.org/#/add/v2/0x6B175474E89094C44Da98b954EedeAC495271d0F/0xdAC17F958D2ee523a2206206994597C13D831ec7/theme=dark"
+                    src="https://app.uniswap.org/#/add/v2/0x6B175474E89094C44Da98b954EedeAC495271d0F/0xdAC17F958D2ee523a2206206994597C13D831ec7?theme=dark"
                     height="700px"
                     width="100%"
                     className="rounded-none xl:rounded-xl shadow-black opacity-80 w-screen h-screen xl:w-full xl:h-full bg-transparent overscroll-auto"
