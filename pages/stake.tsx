be404c6cb86244208f0a847ceb70db0464550245
DAC-info <luca.eich@thedac.info>
2021-12-02 04:23:37 +0900

    hotfix allowance check

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -113,7 +113,7 @@ const Stake: NextPage = () => {
                     <div className="flex flex-col space-y-5 w-full lg:w-7/12">
 
                         <div className="relative flex flex-col space-y-5 h-full items-center justify-between border-t border-l border-opacity-10 shadow-black rounded-xl p-2 sm:p-5 w-full bg-grey-dark bg-opacity-30">
-                            {(!web3Provider || !allowance || chainId !== Chains.MATIC_MAINNET.chainId) && <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-0 backdrop-blur-sm rounded-xl z-20"></div>}
+                            {(!web3Provider || !+allowance || chainId !== Chains.MATIC_MAINNET.chainId) && <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-0 backdrop-blur-sm rounded-xl z-20"></div>}
                             <div className="self-start flex items-center justify-center space-x-2 sm:space-x-4 w-full pr-2 mb-2 z-30">
                                 <img src="/images/mgh_logo.png" className="object-scale-down h-10 sm:h-14 p-1" />
                                 <h3 className="text-gray-300 pb-1.5">$MGH Staking</h3>
@@ -155,7 +155,7 @@ const Stake: NextPage = () => {
                                 </button>
                             )}
 
-                            {web3Provider && !allowance && chainId === Chains.MATIC_MAINNET.chainId && (
+                            {web3Provider && !+allowance && chainId === Chains.MATIC_MAINNET.chainId && (
                                 <button onClick={approve} className="z-30 disabled:opacity-50 disabled:hover:shadow-black disabled:cursor-default mt-4 relative flex justify-center items-center  transition ease-in-out duration-500 shadow-black rounded-xl w-full max-w-md py-3 sm:py-4 group">
                                     <div className="h-full w-full absolute bg-gradient-to-br transition-all ease-in-out duration-300 from-pink-600 to-blue-500 rounded-xl opacity-60 group-hover:opacity-80" />
                                     <span className="pt-1 z-10 text-gray-200 font-medium text-lg sm:text-xl">Approve MGH</span>
