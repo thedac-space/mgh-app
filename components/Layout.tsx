90bf863ed40edb7a88817475b40a1b5650c573f0
mauro guz <mauroguzbejar@gmail.com>
2022-02-24 13:49:54 +0100

    layout mild fix

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,6 +9,7 @@ import WalletButton from "./WalletButton"
 import SmallScreenToolbar from "./SmallScreenToolbar"
 import WalletModal from "./WalletModal";
 import NetworkButton from "./NetworkButton";
+import { OptimizedImage } from "./General";
 
 
 const Layout = ({ children }: any) => {
@@ -29,9 +30,9 @@ const Layout = ({ children }: any) => {
 
                 <SmallScreenToolbar onWalletClick={() => setOpenModal(true)} disconnectWallet={disconnectWallet} web3Provider={web3Provider} chainId={chainId} />
 
-                <div className="hidden xl:flex space-x-10 h-32 w-full items-center justify-between px-5">
+                <div className="hidden xl:flex space-x-10 h-32 w-full items-center justify-between p-5">
                     <a href="/" className="hover:scale-110 transition-all duration-500 ease-in-out ">
-                        <img src="/images/mgh_logo.png" className={` h-16 w-16`} />
+                        <OptimizedImage src="/images/mgh_logo.png" height={64} width={64} />
                     </a>
 
                     <div className="flex space-x-5 items-stretch">
