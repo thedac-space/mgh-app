ae3146db73857679c00881f292fb94f12dfacfcb
mauro guz <mauroguzbejar@gmail.com>
2022-03-18 10:34:11 +0100

    updated types from backend

--- lib/types.ts
+++ lib/types.ts
@@ -1,68 +1,72 @@
-import { Metaverse, Network } from "./enums";
+import { Metaverse, Network } from './enums'
 
 export interface NetworkState {
-    value: Network
+  value: Network
 }
 
 export interface AccountState {
-    connected: boolean,
-    address: string | undefined,
-    chainId: number | undefined
+  connected: boolean
+  address: string | undefined
+  chainId: number | undefined
 }
 
 export interface AddEthereumChainParameter {
-    chainId: string; // A 0x-prefixed hexadecimal string
-    chainName: string;
-    nativeCurrency: {
-        name: string;
-        symbol: string; // 2-6 characters long
-        decimals: 18;
-    };
-    rpcUrls: string[];
+  chainId: string // A 0x-prefixed hexadecimal string
+  chainName: string
+  nativeCurrency: {
+    name: string
+    symbol: string // 2-6 characters long
+    decimals: 18
+  }
+  rpcUrls: string[]
 }
 
-
 export interface IAssetData {
-    name: string
-    symbol: string
-    decimals: number
-    contractAddress?: string
+  name: string
+  symbol: string
+  decimals: number
+  contractAddress?: string
 }
 
 export interface IChainData {
-    name: string,
-    chainId: number,
-    chainIdHex: string,
-    rpcUrl: string,
-    nativeCurrency: IAssetData,
-    blockExplorer: string,
+  name: string
+  chainId: number
+  chainIdHex: string
+  rpcUrl: string
+  nativeCurrency: IAssetData
+  blockExplorer: string
 }
 
 export interface IPoolData {
-    id: number;
-    name: string;
-    APY: number | string;
-    lockingMonth: number;
+  id: number
+  name: string
+  APY: number | string
+  lockingMonth: number
 }
 
 export interface IAPIData {
-    coords: {x: number, y: number}
-    metaverse: Metaverse;
-    name?: string;
-    opensea_link?: string;
-    external_link?: string;
-    url?: string;
-    images?: {
-        image_url: string;
-    };
-    image?: string
-    tokenId: string;
-    prices?: { predicted_price: number }
+  coords: { x: number; y: number }
+  metaverse: Metaverse
+  name?: string
+  opensea_link?: string
+  external_link: string
+  images: {
+    image_url: string
+  }
+  tokenId: string
+  prices?: {
+    predicted_price: number
+    eth_predicted_price: number
+    history: { price: number; timestamp: number }[] | never[]
+    variation_last_four_weeks?: number
+    variation_last_six_months?: number
+    variation_last_week?: number
+  }
 }
 
 export interface IPredictions {
-    ethPrediction: number;
-    usdPrediction: number;
-    sandPrediction?: number;
-    manaPrediction?: number;
+  ethPrediction: number
+  usdPrediction: number
+  sandPrediction?: number
+  manaPrediction?: number
 }
