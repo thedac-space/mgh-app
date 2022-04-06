ebc0253242449d41dcfe49c249f7eecbdb2ca43f
Maurechi <mauroguzbejar@gmail.com>
2022-04-06 18:17:18 +0200

    everything into one single object. Adjusted some types

--- components/Valuation/FloorAndVolumeChart.tsx
+++ components/Valuation/FloorAndVolumeChart.tsx
@@ -1,44 +1,39 @@
-import React, { useEffect, useState, useRef, SetStateAction } from "react";
-import { Metaverse } from "../../lib/enums";
-import { createChart, SingleValueData } from "lightweight-charts";
-import { getValuationDailyData } from "../../lib/FirebaseUtilities";
+import React, { useEffect, useState, useRef } from 'react'
+import { Metaverse } from '../../lib/enums'
+import { createChart, UTCTimestamp } from 'lightweight-charts'
+import { getValuationDailyData } from '../../lib/FirebaseUtilities'
+import { typedKeys } from '../../lib/utilities'
 
 interface IChartValues {
-  time: number;
-  dailyVolume: {
-    ethPrediction: number;
-    usdPrediction: number;
-    metaversePrediction: number;
-  }[];
-  floorPrice: {
-    ethPrediction: number;
-    usdPrediction: number;
-    metaversePrediction: number;
-  }[];
+  time: number
+  dailyVolume: Record<FloorVolumeKeys, number>
+  floorPrice: Record<FloorVolumeKeys, number>
 }
-interface ISymbol {
-
-}
+type FloorVolumeKeys = 'ethPrediction' | 'usdPrediction' | 'metaversePrediction'
 
 const FloorAndVolumeChart = ({ metaverse }: { metaverse: Metaverse }) => {
-  const metaverseGraph = useRef<HTMLDivElement>(null);
-  const [values, setValues] = useState<IChartValues[]>([]);
-  const [symbol, setSymbol] = useState<string>("ETH");
-  const metaverseSymbols = {
-    sandbox: "SAND",
-    decentraland: "MANA",
-    "axie-infinity": "AXS",
-  };
-  const symbolKeys = {ETH:"ethPrediction",USDC:"usdPrediction",[metaverseSymbols[metaverse]]:"metaversePrediction"}
-  const symbols = ["ETH", "USDC", metaverseSymbols[metaverse]];
+  const metaverseGraph = useRef<HTMLDivElement>(null)
+  const [values, setValues] = useState<IChartValues[]>([])
+  const symbolOptions = {
+    ETH: { key: 'ethPrediction' },
+    USDC: { key: 'usdPrediction' },
+    METAVERSE: {
+      key: 'metaversePrediction',
+      sandbox: 'SAND',
+      decentraland: 'MANA',
+      'axie-infinity': 'AXS',
+    },
+  }
+  const [symbol, setSymbol] = useState<keyof typeof symbolOptions>('ETH')
 
   useEffect(() => {
-    (async () => setValues(await getValuationDailyData(metaverse)))();
-  }, [metaverse]);
+    (async () => setValues(await getValuationDailyData(metaverse)))()
+  }, [metaverse])
 
   useEffect(() => {
-    const chart = createChart(metaverseGraph.current as HTMLElement, {
-      width: metaverseGraph.current?.clientWidth,
+    if (!metaverseGraph.current) return
+    const chart = createChart(metaverseGraph.current, {
+      width: metaverseGraph.current.clientWidth,
       height: 197,
       timeScale: {
         fixLeftEdge: true,
@@ -53,90 +48,85 @@ const FloorAndVolumeChart = ({ metaverse }: { metaverse: Metaverse }) => {
         borderVisible: false,
       },
       layout: {
-        backgroundColor: "#131722",
-        textColor: "#d1d4dc",
+        backgroundColor: '#131722',
+        textColor: '#d1d4dc',
       },
       grid: {
         vertLines: {
-          color: "rgba(42, 46, 57, 0)",
+          color: 'rgba(42, 46, 57, 0)',
         },
         horzLines: {
-          color: "rgba(42, 46, 57, 0.6)",
+          color: 'rgba(42, 46, 57, 0.6)',
         },
       },
-    });
+    })
     const areaSeries = chart.addAreaSeries({
-      topColor: "rgba(38,198,218, 0.56)",
-      bottomColor: "rgba(38,198,218, 0.04)",
-      lineColor: "rgba(38,198,218, 1)",
+      topColor: 'rgba(38,198,218, 0.56)',
+      bottomColor: 'rgba(38,198,218, 0.04)',
+      lineColor: 'rgba(38,198,218, 1)',
       lineWidth: 2,
-      title: "Daily Volume",
-    });
+      title: 'Daily Volume',
+    })
 
     const volumeSeries = chart.addHistogramSeries({
-      color: "#26a69a",
-      title: "Floor Price",
+      color: '#26a69a',
+      title: 'Floor Price',
       priceFormat: {
-        type: "volume",
+        type: 'volume',
       },
-      priceScaleId: "",
+      priceScaleId: '',
       scaleMargins: {
         top: 0.8,
         bottom: 0,
       },
-    });
-    
+    })
+
     areaSeries.setData(
       values.map((data: IChartValues) => {
         return {
-          time: new Date(data.time) as any / 1000,
-          value:
-            data.dailyVolume[
-              symbolKeys[symbol]
-            ],
-        } as SingleValueData;
+          time: (data.time / 1000) as UTCTimestamp,
+          value: data.dailyVolume[symbolOptions[symbol].key as FloorVolumeKeys],
+        }
       })
-    );
+    )
     volumeSeries.setData(
       values.map((data: IChartValues) => {
         return {
-          time: new Date(data.time) as any / 1000,
-          value:
-            data.floorPrice[
-              symbolKeys[symbol]
-            ],
-        } as SingleValueData;
+          time: (data.time / 1000) as UTCTimestamp,
+          value: data.floorPrice[symbolOptions[symbol].key as FloorVolumeKeys],
+        }
       })
-    );
+    )
     const resizeGraph = () =>
-      chart.applyOptions({ width: metaverseGraph.current?.clientWidth });
-    window.addEventListener("resize", resizeGraph);
+      chart.applyOptions({ width: metaverseGraph.current?.clientWidth })
+    window.addEventListener('resize', resizeGraph)
     return () => {
-      window.removeEventListener("resize", resizeGraph);
+      window.removeEventListener('resize', resizeGraph)
 
-      chart.remove();
-    };
-  }, [values, symbol]);
-
+      chart.remove()
+    }
+  }, [values, symbol])
 
   return (
-    <div className="max-w-full h-full relative" ref={metaverseGraph}>
-      <div className="absolute top-1 left-1 z-10 flex gap-2">
-        {symbols.map((arrSymbol) => (
+    <div className='max-w-full h-full relative' ref={metaverseGraph}>
+      <div className='absolute top-1 left-1 z-10 flex gap-2'>
+        {typedKeys(symbolOptions).map((arrSymbol) => (
           <button
             className={
-              "gray-box font-semibold  rounded-lg p-2 text-xs text-gray-400" +
+              'gray-box font-semibold  rounded-lg p-2 text-xs text-gray-400' +
               (symbol === arrSymbol
-                ? " text-gray-300 bg-opacity-80 "
-                : " hover:text-gray-300 hover:bg-opacity-80")
+                ? ' text-gray-300 bg-opacity-80 '
+                : ' hover:text-gray-300 hover:bg-opacity-80')
             }
             onClick={() => setSymbol(arrSymbol)}
           >
-            {arrSymbol}
+            {arrSymbol === 'METAVERSE'
+              ? symbolOptions[arrSymbol][metaverse]
+              : arrSymbol}
           </button>
         ))}
       </div>
     </div>
-  );
-};
-export default FloorAndVolumeChart;
+  )
+}
+export default FloorAndVolumeChart
