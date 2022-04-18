aafb27bb3c4e6266df0764a0b665250a9cd7b19d
Maurechi <mauroguzbejar@gmail.com>
2022-04-19 05:26:29 +0800

    changed to 2000 per request

--- lib/heatmap/fetchAtlas.ts
+++ lib/heatmap/fetchAtlas.ts
@@ -8,41 +8,43 @@ export const fetchITRMAtlas = async (
   setLandsLoaded: React.Dispatch<React.SetStateAction<number>>
 ) => {
   const valuationAtlas: Record<string, ValuationTile> = {}
+  const LANDS_PER_REQUEST = 2000
   await Promise.all(
-    [...Array(Math.ceil(heatmapMvOptions[metaverse].lands / 500))].map(
-      async (_, i) => {
-        // 500 lands each time
-        try {
-          // Getting valuations from ITRM
-          const valuationRes = await fetch(
-            `https://services.itrmachines.com/${metaverse}/requestMap?from=${
-              i * 500
-            }&size=500`
-          )
-          const valuations = (await valuationRes.json()) as Record<
-            string,
-            ValuationTile | undefined
-          >
-          // Mapping through those valuations
-          typedKeys(valuations).map((key) => {
-            {
-              // Making a name to fit with our tile map
-              const name =
-                valuations[key]?.coords.x + ',' + valuations[key]?.coords.y
-              // // Setting the land info in the valuation atlas
-              valuationAtlas[name] = valuations[key]!
-            }
-          })
-        } catch (e) {
-          console.log('error', e)
-        }
-        setLandsLoaded((prev) =>
-          heatmapMvOptions[metaverse].lands < prev + 500
-            ? heatmapMvOptions[metaverse].lands
-            : prev + 500
+    [
+      ...Array(
+        Math.ceil(heatmapMvOptions[metaverse].lands / LANDS_PER_REQUEST)
+      ),
+    ].map(async (_, i) => {
+      try {
+        // Getting valuations from ITRM
+        const valuationRes = await fetch(
+          `https://services.itrmachines.com/${metaverse}/requestMap?from=${
+            i * LANDS_PER_REQUEST
+          }&size=${LANDS_PER_REQUEST}`
         )
+        const valuations = (await valuationRes.json()) as Record<
+          string,
+          ValuationTile | undefined
+        >
+        // Mapping through those valuations
+        typedKeys(valuations).map((key) => {
+          {
+            // Making a name to fit with our tile map
+            const name =
+              valuations[key]?.coords.x + ',' + valuations[key]?.coords.y
+            // // Setting the land info in the valuation atlas
+            valuationAtlas[name] = valuations[key]!
+          }
+        })
+      } catch (e) {
+        console.log('error', e)
       }
-    )
+      setLandsLoaded((prev) =>
+        heatmapMvOptions[metaverse].lands < prev + LANDS_PER_REQUEST
+          ? heatmapMvOptions[metaverse].lands
+          : prev + LANDS_PER_REQUEST
+      )
+    })
   )
   return valuationAtlas
 }
