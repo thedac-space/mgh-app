d9814e087fc7709ec5b9a01580b7d61e38f695a1
Maurechi <mauroguzbejar@gmail.com>
2022-06-07 11:13:07 +0200

    create role sign

---
+++ components/Purchase/PurchaseRoleSign.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useAppSelector } from '../../state/hooks'
+
+const PurchaseRoleSign = () => {
+  const { role } = useAppSelector((state) => state.account)
+  console.log({ role })
+  return (
+    /* Role Sign */
+    <div className='m-auto w-fit gray-box bg-opacity-10 mb-8'>
+      <h3 className='text-3xl text-center'>
+        Tier: {role ? role.tier : 'None'}
+      </h3>
+      {role && <p>Active until: {new Date(role.expiration).toDateString()}</p>}
+    </div>
+  )
+}
+
+export default PurchaseRoleSign
