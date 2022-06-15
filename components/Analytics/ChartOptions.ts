96675042bbc52dce3e09cc61c79b0fd50f074363
GabCh15 <gabch15@hotmail.com>
2022-06-14 22:39:16 -0500

    Adding popover to charts

--- components/Analytics/ChartOptions.ts
+++ components/Analytics/ChartOptions.ts
@@ -1,20 +1,32 @@
 export const chartRoutes = [
-  { route: 'avgPriceParcel', label: 'Average Price per Parcel' },
-  { route: 'floorPrice', label: 'Floor Price' },
-  { route: 'avgPriceParcelPerArea', label: 'Average Price per Area' },
-  { route: 'maxPrice', label: 'Max Price' },
-  { route: 'totalNumberOfSales', label: 'Total Sales' },
-  { route: 'stdSalesPrices', label: 'Sales Prices' },
-  { route: 'salesVolume', label: 'Sales Volume' },
+    {
+        route: 'avgPriceParcel',
+        label: 'Average Sale Price per Parcel',
+        description:
+            'This chart shows up the average price per parcel for each day',
+    },
+    {
+        route: 'floorPrice',
+        label: 'Floor Price',
+        description: 'This chart shows the floor price for each day',
+    },
+    {
+        route: 'avgPriceParcelPerArea',
+        label: 'Average Sale Price per SQM',
+        description: '',
+    },
+    { route: 'totalNumberOfSales', label: 'Total Sales', description: '' },
+    { route: 'stdSalesPrices', label: 'Sales Prices', description: '' },
+    { route: 'salesVolume', label: 'Daily Sales Volume', description: '' },
 ]
 
 export const chartSymbolOptions = {
-  ETH: { key: 'ethPrediction' },
-  USDC: { key: 'usdPrediction' },
-  METAVERSE: {
-    key: 'metaversePrediction',
-    sandbox: 'SAND',
-    decentraland: 'MANA',
-    'axie-infinity': 'AXS',
-  },
+    ETH: { key: 'ethPrediction' },
+    USDC: { key: 'usdPrediction' },
+    METAVERSE: {
+        key: 'metaversePrediction',
+        sandbox: 'SAND',
+        decentraland: 'MANA',
+        'axie-infinity': 'AXS',
+    },
 } as const
