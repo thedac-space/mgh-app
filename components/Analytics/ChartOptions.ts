89b71bfa28682b83520a530dff1e540aca911ecb
GabCh15 <gabch15@hotmail.com>
2022-07-13 14:00:03 -0500

    Chart descriptions finish

--- components/Analytics/ChartOptions.ts
+++ components/Analytics/ChartOptions.ts
@@ -16,8 +16,9 @@ export const chartRoutes = [
         description: 'Average sale price for 1 m^2 of LAND for a given timeframe',
     },
     { route: 'totalNumberOfSales', label: 'Total Sales', description: 'Total plots of LAND sold in a given period of time' },
-    { route: 'stdSalesPrices', label: 'Sales Prices', description: '' },
+    { route: 'stdSalesPrices', label: 'Standard Deviation', description: 'Standard deviation of average sales prices' },
     { route: 'salesVolume', label: 'Daily Sales Volume', description: 'LAND sales volume for a given timeframe' },
+    { route: 'maxPrice', label: 'Max Price', description: 'Highest price paid for a plot of LAND in a given timeframe'}
 ]
 
 export const chartSymbolOptions = {
