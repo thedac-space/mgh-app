546dd553149dc20d88b83d0e651debcc962a6efb
Fabian Ruiz <40047900+F4B123@users.noreply.github.com>
2022-07-15 23:09:23 -0500

    Update FirebaseUtilities.ts

--- lib/FirebaseUtilities.ts
+++ lib/FirebaseUtilities.ts
@@ -1,58 +1,54 @@
 import axios from "axios";
-import { initializeApp } from "firebase/app";
-import {
-  doc,
-  getDoc,
-  getFirestore,
-  updateDoc,
-  arrayUnion,
-  arrayRemove,
-  collection,
-  setDoc,
-} from "firebase/firestore/lite";
-import { Score } from "../components/Valuation/LandLikeBox";
+// import { initializeApp } from "firebase/app";
+// import {
+//   doc,
+//   getDoc,
+//   getFirestore,
+//   updateDoc,
+//   arrayUnion,
+//   arrayRemove,
+//   collection,
+//   setDoc,
+// } from "firebase/firestore/lite";
+// import { Score } from "../components/Valuation/LandLikeBox";
 import { Metaverse } from "./metaverse";
 
-const URL = "http://localhost:3001/login/";
+const URL = "http://localhost:3002/firebase/";
 
-// Firebase Init
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE,
-  authDomain: "mgh-app-d6122.firebaseapp.com",
-  projectId: "mgh-app-d6122",
-  storageBucket: "mgh-app-d6122.appspot.com",
-  messagingSenderId: "335800469615",
-  appId: "1:335800469615:web:a90434b4ff8ff6e9c2259a",
-  measurementId: "G-SYMJ1J305Q",
-};
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// // Firebase Init
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE,
+//   authDomain: "mgh-app-d6122.firebaseapp.com",
+//   projectId: "mgh-app-d6122",
+//   storageBucket: "mgh-app-d6122.appspot.com",
+//   messagingSenderId: "335800469615",
+//   appId: "1:335800469615:web:a90434b4ff8ff6e9c2259a",
+//   measurementId: "G-SYMJ1J305Q",
+// };
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
 
 // Getting User Instance (WatchList Lands)
 export async function getUserInfo(walletAddress: string) {
-  const userRef = doc(db, "users", walletAddress);
-  const user = await getDoc(userRef);
-  return user.data();
+  const user = await calls(URL + "getUserInfo", {
+    walletAddress: walletAddress,
+  });
+  return user.res;
 }
 
 // Creating a User
 export async function createUser(walletAddress: string) {
-  const users = collection(db, "users");
-  await setDoc(doc(users, walletAddress), {
-    "sandbox-watchlist": [],
-    "decentraland-watchlist": [],
-    "axie-infinity-watchlist": [],
-  });
+  await calls(URL + "createUser", { walletAddress: walletAddress });
 }
+
 // Add Missing Watchlist in case there's an update/new metaverse and user had already been created
 export async function addMissingWatchlist(
   walletAddress: string,
   watchlist: string
 ) {
-  const users = collection(db, "users");
-console.log(watchlist)
-  await updateDoc(doc(users, walletAddress), {
-    [watchlist]: [],
+  await calls(URL + "addMissingWatchlist", {
+    walletAddress: walletAddress,
+    watchlist: watchlist,
   });
 }
 
@@ -62,22 +58,11 @@ export async function addLandToWatchList(
   walletAddress: string,
   metaverse: Metaverse
 ) {
-  const user = doc(db, 'users', walletAddress)
-  await updateDoc(user, {
-    [metaverse + '-watchlist']: arrayUnion(landId),
-  })
-
-  const updatedData = await getUserInfo(walletAddress)
-  return updatedData
-  // console.log("firing")
-  // const updatedData = await calls(`${URL}addToWatchlist`, {
-  //   address: walletAddress,
-  //   metaverse: metaverse,
-  //   land: landId,
-  //   method: "update",
-  // });
-
-  return updatedData;
+  await calls(URL + "addLandToWatchList", {
+    walletAddress: walletAddress,
+    landId: landId,
+    metaverse: metaverse,
+  });
 }
 
 // Remove Land from User's WatchList
@@ -86,27 +71,25 @@ export async function removeLandFromWatchList(
   walletAddress: string,
   metaverse: Metaverse
 ) {
-  const user = doc(db, "users", walletAddress);
-  await updateDoc(user, {
-    [metaverse + "-watchlist"]: arrayRemove(landId),
+  await calls(URL + "removeLandFromWatchList", {
+    walletAddress: walletAddress,
+    landId: landId,
+    metaverse: metaverse,
   });
 
-  const updatedData = await getUserInfo(walletAddress);
-  return updatedData;
 }
 
 /* Valuation scores  */
 
 //get valuation scores
 export async function getValuationScores(landId: string, metaverse: Metaverse) {
-  try {
-    const land = doc(db, "lands-" + metaverse, landId);
-    const score = (await getDoc(land)).data();
-    if (!score) return await createValuationScore(landId, metaverse);
-    return score;
-  } catch (error) {
-    console.log(error);
-  }
+  console.log(">>get")
+  const score= await calls(URL + "getValuationScores", {
+    landId: landId,
+    metaverse: metaverse,
+  });
+  console.log(">>score",score.res)
+  return score.res
 }
 
 // Creating a valuation score
@@ -114,10 +97,10 @@ export async function createValuationScore(
   landId: string,
   metaverse: Metaverse
 ) {
-  const land = collection(db, "lands-" + metaverse);
-  await setDoc(doc(land, landId), {
-    likes: [],
-    dislikes: [],
+  console.log("likes")
+  await calls(URL + "createValuationScore", {
+    landId: landId,
+    metaverse: metaverse,
   });
 }
 
@@ -127,19 +110,10 @@ export async function likeLand(
   address: string,
   metaverse: Metaverse
 ) {
-  const land = doc(db, "lands-" + metaverse, landId);
-  const landData = (await getDoc(land)).data() as Score | undefined;
-  // If user already liked then take that like away
-  if (landData?.likes.includes(address))
-    return await updateDoc(land, {
-      likes: arrayRemove(address),
-    });
-
-  await updateDoc(land, {
-    likes: arrayUnion(address),
-  });
-  await updateDoc(land, {
-    dislikes: arrayRemove(address),
+  await calls(URL + "likeLand", {
+    landId: landId,
+    address:address,
+    metaverse: metaverse,
   });
 }
 
@@ -149,41 +123,27 @@ export async function dislikeLand(
   address: string,
   metaverse: Metaverse
 ) {
-  const land = doc(db, "lands-" + metaverse, landId);
-  const landData = (await getDoc(land)).data() as Score | undefined;
-  // If user already disliked then take that dislike away
-  if (landData?.dislikes.includes(address))
-    return await updateDoc(land, {
-      dislikes: arrayRemove(address),
-    });
-
-  await updateDoc(land, {
-    dislikes: arrayUnion(address),
-  });
-  await updateDoc(land, {
-    likes: arrayRemove(address),
+  await calls(URL + "dislikeLand", {
+    landId: landId,
+    address:address,
+    metaverse: metaverse,
   });
 }
 
 // Retrieve valuation daily data
 export async function getValuationDailyData(metaverse: Metaverse) {
-  const valuesDoc = doc(db, "valuationGraphs", metaverse);
-  const values = (await getDoc(valuesDoc)).data()?.values;
-  return values;
+  const values = await calls(URL + "getValuationDailyData", {
+    metaverse: metaverse,
+  });
+  return values.res;
 }
 
 async function calls(URL: string, body: object) {
-  // let {data} = await axios.post(URL, body, {
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     Accept: "application/json",
-  //   },
-  // });
-
-  let { data } = await axios.get(URL)
-
-  console.log(">>api is working", data);
+  let { data } = await axios.post(URL, body, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
   return data;
 }
-
-calls(`${URL}test`, {});
