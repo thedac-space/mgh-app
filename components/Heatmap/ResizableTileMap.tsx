4a12a7c5a353dfad7cb6fab606ccfbb74db2a2f5
GabCh15 <info@thedac.info>
2022-11-08 10:39:21 -0500

    Tilemap fix

--- components/Heatmap/ResizableTileMap.tsx
+++ components/Heatmap/ResizableTileMap.tsx
@@ -1,18 +1,14 @@
 import * as React from 'react'
 // import { AutoSizer } from 'react-virtualized'
 
-import { TileMap } from './TileMap'
-import { Props } from './TileMap.types'
+/* import { TileMap } from './TileMap'
+import { Props } from './TileMap.types' */
 
-type ResizableProps = Omit<Props, 'width' | 'height'>
-const resizableDefaultProps = { ...TileMap.defaultProps }
-// delete resizableDefaultProps.width
-// delete resizableDefaultProps.height
 
-export class ResizableTileMap extends React.PureComponent<ResizableProps> {
-  static defaultProps = resizableDefaultProps
+export class ResizableTileMap  {
+
   render() {
     // return <AutoSizer>{props => <TileMap {...props} {...this.props} />}</AutoSizer>
-    return <TileMap {...this.props} />
+    return <></>
   }
 }
